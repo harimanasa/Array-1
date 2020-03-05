879141ff77ee566e802f107e72fd257d713c0967
Manasa Hari <harimanasa17@gmail.com>
2020-03-04 22:37:39 -0800

    Create OptimalProductExceptSelf.java

---
+++ OptimalProductExceptSelf.java
@@ -0,0 +1,27 @@
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        //validate
+        if(nums==null || nums.length==0) return nums;
+        
+        
+        //logic
+        int ans[] = new int[nums.length];
+        for(int i=0; i<nums.length; i++){ans[i]=1;} //initialization
+        //System.out.println(Arrays.toString(ans));
+        
+        int temp=1;
+        for(int i=0; i<nums.length; i++){
+            ans[i] *= temp;
+            temp *= nums[i];
+        }
+        temp=1;
+        for(int i=nums.length-1; i>=0; i--){
+            ans[i] *= temp;
+            temp *= nums[i];
+        }
+        
+        // System.out.println(Arrays.toString(ans));
+        return ans;
+    }
+}
